d926ff210ec8c58bd09b90d134d26f04ef180e23
RetroPunch96 <rasel666@gmail.com>
2025-04-09 13:55:47 +0300

    bot.py

--- crypto-trading-bot/bot.py
+++ crypto-trading-bot/bot.py
@@ -14,33 +14,3 @@ class CryptoTradingBot:
 if __name__ == "__main__":
     bot = CryptoTradingBot()
     asyncio.run(bot.run())
-
-from core.trading_system import TradingSystem
-from telegram.bot import TelegramBot
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class CryptoBot:
-    def __init__(self):
-        self.trading_system = TradingSystem()
-        self.telegram_bot = TelegramBot(
-            token=os.getenv("TELEGRAM_TOKEN"),
-            trading_bot=self
-        )
-    
-    def start(self):
-        self.trading_system.start()
-        self.telegram_bot.start()
-    
-    def stop(self):
-        self.trading_system.stop()
-        self.telegram_bot.stop()
-
-if __name__ == "__main__":
-    bot = CryptoBot()
-    try:
-        bot.start()
-    except KeyboardInterrupt:
-        bot.stop()
